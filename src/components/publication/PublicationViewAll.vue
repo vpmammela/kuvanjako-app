11dfeaad6c232edb380a320ddaf4b22a76882943
Veli-Pekka Mämmelä <vpmammela@icloud.com>
2022-03-28 19:51:42 +0300

    first commit

---
+++ src/components/publication/PublicationViewAll.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const message = 'Tämä on testi'
+const loggedIn = false
+
+</script>
+
+<template>
+<h1>PublicationViewAll OK!</h1>
+<p>{{ message }}</p>
+
+<h1 v-if="loggedIn">loggedIn on true!</h1>
+<h1 v-else>loggedIn on false.... :(</h1>
+</template>
+
+<style>
+body {
+    margin: 30px;
+}
+</style>
